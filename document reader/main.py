5a6c855ed23db917e3239ed9e8fbe5a47b219785
Max Lepyavko <maxlepyavko@gmail.com>
2020-02-16 14:31:30 +0700

    Change function for text extraction of pdf files

--- document reader/main.py
+++ document reader/main.py
@@ -1,41 +1,7 @@
-import io
-
-from pdfminer.converter import TextConverter
-from pdfminer.pdfinterp import PDFPageInterpreter
-from pdfminer.pdfinterp import PDFResourceManager
-from pdfminer.pdfpage import PDFPage
-import wordninja
-import nltk
-
-
-def extract_text_from_pdf(pdf_path):
-    resource_manager = PDFResourceManager()
-    fake_file_handle = io.StringIO()
-    converter = TextConverter(resource_manager, fake_file_handle)
-    page_interpreter = PDFPageInterpreter(resource_manager, converter)
-
-    with open(pdf_path, 'rb') as fh:
-        for page in PDFPage.get_pages(fh,
-                                      caching=True,
-                                      check_extractable=True):
-            page_interpreter.process_page(page)
-
-        text = fake_file_handle.getvalue()
-
-
-    converter.close()
-    fake_file_handle.close()
-
-    if text:
-        return text
+import textract
 
 
 if __name__ == '__main__':
-    text = extract_text_from_pdf('../data/raw/example1.pdf')
-    texts = text.split('.')
-    lm = wordninja.LanguageModel("dictionary.tar.gz")
+    text = str(textract.process('../data/raw/example1.pdf'), encoding="utf8")
 
-    for i in texts:
-            d = lm.split(i)
-            if d != []:
-                print(d)
+    print(text)
