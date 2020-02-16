6b2d9b0543779c740f5d5683e60d7283523c256a
Max Lepyavko <maxlepyavko@gmail.com>
2020-02-16 18:55:29 +0700

    Change pdf parser and find function for sentece tokenization

--- document reader/main.py
+++ document reader/main.py
@@ -1,7 +1,20 @@
 import textract
+import nltk
+
+SYMBOLS = ["\n", "\x0c", "\x0b", "\r", "\t", ""]
+
+
+def get_text_from_pdf(filepath):
+    text_ = str(textract.process(filepath), encoding="utf8")
+    for symbol in SYMBOLS:
+        text_ = text_.replace(symbol, " ")
+
+    text_ = text_.lower()
+    return text_
 
 
 if __name__ == '__main__':
-    text = str(textract.process('../data/raw/example1.pdf'), encoding="utf8")
+    nltk.download("punkt")
+    text = get_text_from_pdf('../data/raw/example1.pdf')
 
-    print(text)
+    print(nltk.sent_tokenize(text, language='english'))
