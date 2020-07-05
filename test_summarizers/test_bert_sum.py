b1228a35119ee82753b66a2b916dfd044f814cb7
Maxim Lepiavko <mlepiavko@issart.com>
2020-07-05 20:57:07 +0700

    BertSum results

--- test_summarizers/test_bert_sum.py
+++ test_summarizers/test_bert_sum.py
@@ -1,20 +1,54 @@
-from summarizer import Summarizer
+# BERT summarizer
+from summarizer import Summarizer as BertSummarizer
+
+# Metrics
+from rouge import Rouge
+
+# Sumy imports
+from sumy.summarizers.reduction import ReductionSummarizer
+from sumy.parsers.plaintext import PlaintextParser
+from sumy.nlp.tokenizers import Tokenizer
+from sumy.nlp.stemmers import Stemmer
+from sumy.utils import get_stop_words
+
+# xml_reader
 from document_reader.summary_reference_reader import read_string_from_xml
 from document_reader.summary_reference_reader import DATA_PATH
 from document_reader.summary_reference_reader import read_original_text_and_reference_from_filepath
 from document_reader.summary_reference_reader import get_folderpaths
 
-from rouge import Rouge
+# Progress bar
+from progressbar import progressbar
 
-model = Summarizer()
-rouge = Rouge()
+# Errors
+from xml.etree.ElementTree import ParseError
 
-for path in get_folderpaths(DATA_PATH):
-    try:
-        summary_etalon, reference = read_original_text_and_reference_from_filepath(path)
+
+if __name__ == '__main__':
+    model = BertSummarizer()
+    scorer = Rouge()
+
+    summary_references = []
+    summaries = []
+
+    folder_paths = get_folderpaths(DATA_PATH)
+    n_folders = len(folder_paths)
+
+    for n_paper in progressbar(range(n_folders)):
+        path = folder_paths[n_paper]
+
+        try:
+            summary_reference, reference = read_original_text_and_reference_from_filepath(path)
+        except ParseError:
+            continue
+        except FileNotFoundError:
+            continue
+
         result = model(reference, min_length=250)
         summary = ' '.join(result)
 
-        print(rouge.get_scores(summary, summary_etalon))
-    except:
-        continue
+        summary_references.append(summary_reference)
+        summaries.append(summary)
+
+    metrics = scorer.get_scores(summaries, summary_references, avg=True, ignore_empty=True)
+    print(metrics)
